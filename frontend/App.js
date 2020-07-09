93e36f6380f4a00ec5f553c708d6a1314e1f3b46
SaxenaKartik <kartiksaxena500@gmail.com>
2020-07-10 05:21:39 +0530

    Removed the frontend folder

--- frontend/App.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-import { TouchableOpacity, StyleSheet, Text, View } from 'react-native';
-import {AppContainer} from './config/routes.js';
-
-export default class App extends Component {
-  render() {
-    return <AppContainer />;
-  }
-}
--- frontend/config/routes.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { createAppContainer } from "react-navigation";
-import {AppNavigator} from './navigators/AppSwitchNavigator';
-
-
-export const AppContainer = createAppContainer(AppNavigator);
